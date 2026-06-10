f871688f2116fa45233da8054a9ea951cfa41bcb
wang <wangminhong73@gmail.com>
2026-06-10 23:55:39 +0800

    Add limitbuy and sell

--- include/order_book.h
+++ include/order_book.h
@@ -15,26 +15,27 @@ struct Limit {
     uint64_t price;
     uint64_t size;
     uint64_t total_volume;
-    Limit *parent;
-    Limit *left;
-    Limit *right;
-    mystd::unique_ptr<Order> head;
-    mystd::unique_ptr<Order> tail;
+    Limit *parent = nullptr;
+    Limit *left = nullptr;
+    Limit *right = nullptr;
+    Order *head = nullptr;
+    Order *tail = nullptr;
 };
 
 struct Order {
     AddOrder order;
-    uint64_t entry_time;
-    uint64_t exit_time;
-    mystd::unique_ptr<Order> next;
-    Order *prev;
-    Limit *parent;
+    Order *next = nullptr;
+    Order *prev = nullptr;
+    Limit *parent = nullptr;
 };
 class OrderBook {
 public:
-    inline void clear();
-    inline void limit_sell(uint64_t order_id, uint64_t price, uint64_t amount);
-    inline void limit_buy(uint64_t order_id, uint64_t price, uint64_t amount);
+    void clear();
+    void process(AddOrder &);
+    void process(CancelOrder &);
+    void process(Trade &);
+    void limit_sell(AddOrder &processed_sell_order);
+    void limit_buy(AddOrder &processed_buy_order);
     Order &get(uint64_t order_id) const;
     void market_sell();
     void market_buy();
@@ -50,7 +51,7 @@ private:
                                                  sizeof(buybuffer_)};
     BST<Limit> sell_;
     BST<Limit> buy_;
-    Limit low_sell_;
-    Limit highest_buy_;
-    std::unordered_map<uint64_t, Limit *> order_map_;
+    Limit *low_sell_;
+    Limit *highest_buy_;
+    std::unordered_map<uint64_t, Order *> order_map_;
 };
--- src/order_book.cpp
+++ src/order_book.cpp
@@ -1,10 +1,63 @@
 #include "order_book.h"
+#include "protocol.h"
 
-inline void OrderBook::clear() {};
-inline void OrderBook::limit_sell(uint64_t order_id, uint64_t price,
-                                  uint64_t amount) {};
-inline void OrderBook::limit_buy(uint64_t order_id, uint64_t price,
-                                 uint64_t amount) {};
+void OrderBook::process(AddOrder &order) {
+    if (order.buyorsell == 'B') {
+        OrderBook::limit_buy(order);
+    } else {
+        OrderBook::limit_sell(order);
+    }
+};
+void OrderBook::process(CancelOrder &) {};
+void OrderBook::process(Trade &) {};
+
+void OrderBook::clear() {};
+void OrderBook::limit_sell(AddOrder &processed_sell_order) {
+    Limit tmp{static_cast<uint64_t>(processed_sell_order.price), 0, 0};
+    if (auto *limit_node = sell_.search(&tmp); limit_node != nullptr) {
+        void *mem = pool_sell.allocate(sizeof(Order), alignof(Order));
+        limit_node->tail->next = new (mem)
+            Order{processed_sell_order, {}, limit_node->tail, limit_node};
+        limit_node->tail = limit_node->tail->next;
+        limit_node->size++;
+        limit_node->total_volume += processed_sell_order.amount;
+        order_map_[processed_sell_order.order_id] = limit_node->tail;
+    } else {
+        void *mem = pool_sell.allocate(sizeof(Limit), alignof(Limit));
+        void *mem2 = pool_sell.allocate(sizeof(Order), alignof(Order));
+        auto new_limit =
+            new (mem) Limit{static_cast<uint64_t>(processed_sell_order.price),
+                            1, processed_sell_order.amount};
+        new_limit->head =
+            new (mem2) Order{processed_sell_order, {}, {}, new_limit};
+        new_limit->tail = new_limit->head;
+        sell_.insert(new_limit);
+        order_map_[processed_sell_order.order_id] = new_limit->head;
+    }
+};
+void OrderBook::limit_buy(AddOrder &processed_buy_order) {
+    Limit tmp{static_cast<uint64_t>(processed_buy_order.price), 0, 0};
+    if (auto *limit_node = buy_.search(&tmp); limit_node != nullptr) {
+        void *mem = pool_buy.allocate(sizeof(Order), alignof(Order));
+        limit_node->tail->next = new (mem)
+            Order{processed_buy_order, {}, limit_node->tail, limit_node};
+        limit_node->tail = limit_node->tail->next;
+        limit_node->size++;
+        limit_node->total_volume += processed_buy_order.amount;
+        order_map_[processed_buy_order.order_id] = limit_node->tail;
+    } else {
+        void *mem = pool_buy.allocate(sizeof(Limit), alignof(Limit));
+        void *mem2 = pool_buy.allocate(sizeof(Order), alignof(Order));
+        auto new_limit =
+            new (mem) Limit{static_cast<uint64_t>(processed_buy_order.price), 1,
+                            processed_buy_order.amount};
+        new_limit->head =
+            new (mem2) Order{processed_buy_order, {}, {}, new_limit};
+        new_limit->tail = new_limit->head;
+        buy_.insert(new_limit);
+        order_map_[processed_buy_order.order_id] = new_limit->tail;
+    }
+}
 Order &OrderBook::get(uint64_t order_id) const {};
 void OrderBook::market_sell() {};
 void OrderBook::market_buy() {};
